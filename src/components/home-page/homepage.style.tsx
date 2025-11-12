427461c5eb82be36cc643fa5e3af4f68e705a48f
Mpho Cindi <mphocindi@Mphos-MacBook-Air.local>
2025-11-12 02:34:52 +0200

    working on home page

---
+++ src/components/home-page/homepage.style.tsx
@@ -0,0 +1,31 @@
+export const HomePageContainer: React.CSSProperties = {
+  display: "flex",
+  backgroundColor: "#e2a9f1"
+}
+
+export const buttonStyle: React.CSSProperties = {
+    backgroundColor: "#FFCC00",
+    color: "#000",
+    border: "none",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontWeight: "bold",
+}
+
+export const LeftContainer: React.CSSProperties = {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: "20px",
+
+}
+
+export const RightContainer: React.CSSProperties = {
+    flex: 1,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "20px",
+}
--- src/components/home-page/homepage.tsx
+++ src/components/home-page/homepage.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { buttonStyle, HomePageContainer, LeftContainer , RightContainer} from "./homepage.style";
+import logo from "../../assets/homepage.png";
 
 
 export const HomePage: React.FC = () => {
+
+    const learnmore = () => {
+        console.log("I was clicked")
+    }
+    
    return (
-    <h1>hello</h1>
+    <>
+    <div style={HomePageContainer}>
+    <div style={LeftContainer}>
+        <h1>Growing in Faith, Love and Learning</h1>
+        
+        <button style={buttonStyle} onClick={learnmore}>
+            Learn More
+        </button>
+    </div>
+
+    <div style={RightContainer}>
+        <img src={logo} alt="The logo" style={{width: '200px'}} />
+    </div>
+    </div>
+    </>
     );
 };
 
